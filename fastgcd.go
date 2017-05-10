61a93587196873ecf25b663faa7f8e001ae35e90
Elise <eyxue@mit.edu>
2017-05-09 20:26:59 -0400

    changed to big int

--- fastgcd.go
+++ fastgcd.go
@@ -1,15 +1,16 @@
 package main
 
-import ("fmt"
- 		"bufio"
- 		"os"
- 		"strconv"
- 	
- 		
+import (
+    "fmt"
+	"bufio"
+	"os"
+	// "strconv"
+ 	"math/big"
+    // "log"
 )
 
-func input_file (filename string) []int {
-	output := []int{}
+func input_file (filename string) []big.Int {
+	output := []big.Int{}
 	file, err := os.Open(filename)
     if err != nil {
         fmt.Println(err)
@@ -18,11 +19,15 @@ func input_file (filename string) []int {
 
     scanner := bufio.NewScanner(file)
     for scanner.Scan() {
-        line, err := strconv.Atoi(scanner.Text())
+        text := scanner.Text()
+        // line, err:= strconv.Atoi(text)
         if err != nil {
         	fmt.Println(err)
     	}
-        output = append(output, line)
+        newnum := new(big.Int)
+        newnum.SetString(text, 16)
+        fmt.Println(newnum)
+        output = append(output, *newnum)
     }
 
     if err := scanner.Err(); err != nil {
@@ -31,7 +36,7 @@ func input_file (filename string) []int {
     return output
 }
 
-func output_file(level_filename string, inputs []int) {
+func output_file(level_filename string, inputs []big.Int) {
     f, err := os.Create(level_filename)
     if err != nil {
         fmt.Println(err)
@@ -39,7 +44,7 @@ func output_file(level_filename string, inputs []int) {
     defer f.Close()
     w := bufio.NewWriter(f)
     for _, line := range inputs {
-        content := strconv.Itoa(line) + "\r\n" 
+        content := line.String() + "\r\n" 
         fmt.Fprint(w, content)
     }
     w.Flush()
@@ -52,14 +57,16 @@ func product_tree() int{
 		level_filename := fmt.Sprintf("p%d.txt", level)
 		output_file(level_filename, inputs)
     	if len(inputs) == 1{
-    		inputs = []int{}
+    		inputs = []big.Int{}
     	} else{
-		level_vec := []int{}
+		level_vec := []big.Int{}
 		for i:= 0; i<len(inputs); i += 2 {
 			if i+1 == len(inputs) {
 				level_vec = append(level_vec, inputs[i])
 			} else {
-				level_vec = append(level_vec, inputs[i] * inputs[i+1])
+                prod := new(big.Int)
+                prod.Mul(&inputs[i], &inputs[i+1])
+				level_vec = append(level_vec, *prod)
 			}
 		}
 		inputs = level_vec
@@ -74,11 +81,19 @@ func remainder_tree(level int){
 	for level > 0 {
         level = level - 1;
         next_level := input_file(fmt.Sprintf("p%d.txt", level));
-        output_level := []int{}
+        output_level := []big.Int{}
         for i := 0; i < len(current_level); i++ {
-            output_level = append(output_level, current_level[i] % (next_level[2*i] * next_level[2*i]))
+            sq := new(big.Int)
+            sq.Mul(&next_level[2*i],&next_level[2*i])
+            mod := new(big.Int)
+            mod.Mod(&current_level[i], sq)
+            output_level = append(output_level, *mod)
             if 2*i + 1 != len(next_level) {
-                output_level = append(output_level, current_level[i] % (next_level[2*i + 1] * next_level[2*i + 1]))
+                sq2 := new(big.Int)
+                sq2.Mul(&next_level[2*i + 1],&next_level[2*i + 1])
+                mod2 := new(big.Int)
+                mod2.Mod(&current_level[i], sq2)
+                output_level = append(output_level, *mod2)
             }
         }
         level_filename := fmt.Sprintf("r%d.txt", level)
@@ -90,12 +105,18 @@ func remainder_tree(level int){
 func get_results() {
     input_nums:= input_file("p0.txt")
     modded_nums:= input_file("r0.txt")
-    results := []int{}
-    vulnerable := []int{}
+    results := []big.Int{}
+    vulnerable := []big.Int{}
     for i := 0; i < len(input_nums); i++ {
-        div_num := modded_nums[i]/input_nums[i]
-        gcd := GCD(div_num, input_nums[i])
-        if (gcd != 1) {
+        div_num := new(big.Int)
+        div_num.Div(&modded_nums[i], &input_nums[i])
+        gcd := GCD(*div_num, input_nums[i])
+        one := big.NewInt(1)
+        // log.Debug(one)
+        fmt.Print(one)
+        fmt.Print(*one)
+        // one.setInt(1)
+        if (one.Cmp(&gcd)!=0) {
             vulnerable = append(vulnerable, input_nums[i])
         }
         results = append(results, gcd)
@@ -104,9 +125,11 @@ func get_results() {
     output_file("results.txt", results)
 }
 
-func GCD(a, b int) int {
-    for b != 0 {
-        a, b = b, a%b
+func GCD(a, b big.Int) big.Int {
+    zero := big.NewInt(0)
+    mod := new(big.Int)
+    for (zero.Cmp(&b) != 0) {
+        a, b = b, *(mod.Mod(&a, &b))
     }
     return a
 }
